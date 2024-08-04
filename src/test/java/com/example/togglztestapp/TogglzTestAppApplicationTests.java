29a3630268efdc8a9e1a421ce8d1877b246383c0
Georgii Lvov <georgii.lvov@telekom.com>
2024-08-04 18:24:33 +0300

    Initial commit

---
+++ src/test/java/com/example/togglztestapp/TogglzTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.togglztestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogglzTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
